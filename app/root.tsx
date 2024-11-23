cf6115a341710f644dd3216e976fc92ad4172ffa
Marcin Zaremski <marcinzara@gmail.com>
2024-11-23 13:13:55 +0100

    setup paralaxy stars background

--- app/root.tsx
+++ app/root.tsx
@@ -8,7 +8,8 @@ import {
 import { Analytics } from "@vercel/analytics/react";
 import { Theme } from "@radix-ui/themes";
 import radixStyles from "@radix-ui/themes/styles.css?url";
-import styles from "~/main.css?url";
+import styles from "~/star-parallax.css?url";
+import stylesStarParallax from "~/main.css?url";
 import FontStyles from "@fontsource/lexend/index.css?url";
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -30,6 +31,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
           scaling="110%"
           style={{ borderRadius: "var(--radius-6)" }}
         >
+          <div id="stars"></div>
+          <div id="stars2"></div>
+          <div id="stars3"></div>
           {children}
           <ScrollRestoration />
           <Scripts />
@@ -48,6 +52,7 @@ export function links() {
   return [
     { rel: "stylesheet", href: radixStyles },
     { rel: "stylesheet", href: FontStyles },
+    { rel: "stylesheet", href: stylesStarParallax },
     { rel: "stylesheet", href: styles },
   ];
 } 
