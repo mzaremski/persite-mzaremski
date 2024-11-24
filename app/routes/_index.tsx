12ec1ad145b76f4e9efa5574f49fd55c8e382d31
Marcin Zaremski <marcinzara@gmail.com>
2024-11-24 18:30:15 +0100

    style accordion and project buttons, style main card section in general

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -14,69 +14,79 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <Box style={{ background: "var(--gray-a2)", height: "100vh", width: "100vw", zIndex: 100 }}>
+    <Box style={{ background: "var(--gray-a2)", height: "100vh", width: "100vw", zIndex: 100, paddingTop: '100px' }}>
       <Container size="2">
-        <Section>
           <Card size="5">
-            <Section>
-              <Flex gap="2" align="center">
+            <Section size="1">
+              <Flex gap="4" align="center">
                 <Avatar
                   src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
                   fallback="A"
-                  size="4"
+                  size="7"
                 />
-                <h1 style={{ fontSize: '2.5rem', margin: '0' }}>Marcin Zaremski</h1>
+                <div>
+                  <h1 style={{ fontSize: '2.5rem', margin: '0' }}>Marcin Zaremski</h1>
+                  <span style={{ fontSize: '1.5rem'}}>
+                    A<TypeAnimation
+                      speed={2}
+                      repeat={Infinity}
+                      sequence={[
+                        "\ developer",
+                        "n indie hacker",
+                        "\ freelancer",
+                        "n entrepreneur",
+                        "\ photograper (a bit)",
+                        "\ surfer",
+                        "\ cyclist",
+                      ]}
+                    />
+                  </span>
+                </div>
               </Flex>
-              <span style={{ fontSize: '1.5rem' }}>
-                A<TypeAnimation
-                  speed={2}
-                  repeat={Infinity}
-                  sequence={[
-                    "\ developer",
-                    "n indie hacker",
-                    "\ freelancer",
-                    "n entrepreneur",
-                    "\ photograper (a bit)",
-                    "\ surfer",
-                    "\ cyclist",
-                  ]}
-                />
-              </span>
-              <p>
+
+              <p style={{ paddingTop: '1em' }}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim rem cumque iusto animi, odio numquam veritatis illo blanditiis tempore ipsam.
               </p>
             </Section>
-            
-            <Separator size="4" />
 
-            <Section>
+            <Section size="1">
               <h2>Work with me as:</h2>
               <AccordionDemo/>
             </Section>
 
-            <Separator size="4"/>
-
-            <Section>
+            <Section size="1">
               <h2>Projects</h2>
-              <ul>
-                <li>
-                  LingoReaders <Badge color="gray"><Spinner size="1" style={{ display: 'inline-block', }} />Q2 2024</Badge>
+              <ul className="project_list">
+                <li className="project_list_item">
+                  <a href="https://mzaremski.com">
+                    <span>LingoReaders</span>
+                    <Badge color="gray"><Spinner size="1" style={{ display: 'inline-block', }} />Q2 2024</Badge>
+                  </a>
+                  </li>
+                <li className="project_list_item">
+                  <a href="https://mzaremski.com">
+                    Propsify <Badge color="gray"><Spinner size="1" style={{ display: 'inline-block', }} />Q1 2024</Badge>
+                  </a>
+                  </li>
+                <li className="project_list_item">
+                  <a href="https://mzaremski.com">
+                    Persite <Badge color="green">2024 | Free</Badge>
+                  </a>
+                  </li>
+                <li className="project_list_item">
+                  <a href="https://mzaremski.com">
+                    GpxOverlay <Badge color="green">1k$/m</Badge>
+                  </a>
+                  </li>
+                <li className="project_list_item">
+                  <a href="https://mzaremski.com">
+                    Ikonate <Badge color="indigo">2018 | Free</Badge>
+                  </a>
                 </li>
-                <li>
-                  Propsify <Badge color="gray"><Spinner size="1" style={{ display: 'inline-block', }} />Q1 2024</Badge>
-                </li>
-                <li>
-                  Persite <Badge color="green">2024 | Free</Badge>
-                </li>
-                <li>
-                  GpxOverlay <Badge color="green">1k$/m</Badge>
-                </li>
-                <li>
-                  Ikonate <Badge color="indigo">2018 | Free</Badge>
-                </li>  
               </ul>
             </Section>
 
+            {/* // TODO: Change to Flex */}
             <Box width="100%" height="60px" style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '0px', position: 'absolute', bottom: '0', left: '0', borderTop: '1px solid var(--gray-a6)' }}>
               <Box style={{borderRight: '1px solid var(--gray-a6)', textAlign: 'center' }}>
                 <a href="https://linkedin.com" style={{ color: 'var(--gray-a11)', width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', scale: '1.6' }}>
@@ -100,9 +110,8 @@ export default function Index() {
               </Box>
             </Box>
           </Card>
-        </Section>
 
-        <Section style={{ zIndex: 100 }}>
+        <Section>
           <h1>Blog</h1>
           {/* <Card size="1">
             <h2>Lorem ipsum dolor sit.</h2>
