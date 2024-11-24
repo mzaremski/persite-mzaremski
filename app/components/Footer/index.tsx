ea69861d6827658b804726a3d258436e9303cd53
Marcin Zaremski <marcinzara@gmail.com>
2024-11-24 18:40:30 +0100

    add persite link

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,7 +1,10 @@
+import { Link } from "@remix-run/react";
+
 export default function Footer() {
   return (
     <div style={{ position: 'absolute', bottom: '0', width: '100%', textAlign: 'center', fontSize: '0.9rem', paddingBottom: '1rem' }}>
-      Get one for you – <a href="https://mzaremski.com/persite?utm_source=persite">Persite</a>
+      Get one for you – <Link to="/persite">Persite</Link>
+      {/* Get one for you – <a href="https://mzaremski.com/persite?utm_source=persite">Persite</a> */}
     </div>
   );
 }
---
+++ app/routes/persite.tsx
@@ -0,0 +1,3 @@
+export default function Persite() {
+  return <div>Oh, I didn't expect you are here. Although the website is made by Persite boilerplate. I will let you know how to use it soon!</div>;
+}
