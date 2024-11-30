b0f5261966927f1d928157625b4378ee9cce76e1
Marcin Zaremski <marcinzara@gmail.com>
2024-11-30 22:23:24 +0100

    generate og images

--- app/routes/og.tsx
+++ app/routes/og.tsx
@@ -1,13 +1,54 @@
 import { ImageResponse } from '@vercel/og';
-import Header from '~/components/Header';
-import { Card } from '@radix-ui/themes';
+import type { LoaderFunctionArgs } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import persiteData from '../../persite-data';
+import { Box, Card, Flex } from '@radix-ui/themes';
+import radixStyles from "@radix-ui/themes/styles.css?url";
 
-export const loader = async () => {
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+
+  const fontColor = persiteData.theme.mode === 'dark' ? '#ffffff' : '#000000';
+  const shadowColor = persiteData.theme.mode === 'dark' ? 'rgba(0, 0, 0, 0.15)' : 'rgba(255, 255, 255, 0.15)';
+  const backgroundImage = `http://localhost:5173/ogbackground-${persiteData.theme.mode}.jpg`;
+  
   return new ImageResponse(
     (
-      <Card style={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <Header ogImage={true} />
-      </Card>
+      <div
+      style={{
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+      >
+        <img src={backgroundImage} style={{position: 'absolute'}}/>
+        <div style={{display: 'flex', width: '300px', height: '300px', borderRadius: '100%', overflow: 'hidden', position: 'relative', boxShadow: `0 0 40px ${shadowColor}`}}>
+          <img src="http://localhost:5173/avatar.jpg" alt="" style={{position: 'absolute', width: '100%', height: '100%', objectFit: 'cover', transform: 'translate(-50%, -50%)', top: '50%', left: '50%'}}/>
+        </div>
+        <div style={{display: 'flex', height: '300px', flexDirection: 'column', justifyContent: 'center', paddingLeft: '40px', textShadow: `0 0 40px ${shadowColor}`}}>
+          <h1
+            style={{
+              fontSize: '60px',
+              fontWeight: 'bold',
+              color: fontColor,
+              margin: '0'
+            }}
+          >
+            {persiteData.name}
+          </h1>
+          <span
+            style={{
+              fontSize: '35px',
+              color: fontColor,
+            }}
+          >
+            {persiteData.slogan}
+          </span>
+        </div>
+    </div>
     ),
     {
       width: 1200,
@@ -15,3 +56,10 @@ export const loader = async () => {
     }
   );
 };
+
+/*
+* Created with https://www.css-gradient.com
+* Gradient link: https://www.css-gradient.com/?c1=010101&c2=282828&gt=r&gd=dtt
+*/
+// background: #010101;
+// background: radial-gradient(at center top, #010101, #282828);
--- persite-data.ts
+++ persite-data.ts
@@ -1,4 +1,9 @@
 export default {
+  name: 'Marcin Zaremski',
+  slogan: 'Freelancer & Indie Hacker',
   pageTitle: "Marcin Zaremski | Freelancer & Indie Hacker",
   pageDescription: "Not only dev. Also entrepreneur, indie hacker, photographer, surfer and cyclist.",
+  theme: {
+    mode: 'dark',
+  }
 }
