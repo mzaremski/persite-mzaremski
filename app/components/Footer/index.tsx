d4b49ac3d92bca8b5e61b0f38c0da4e094bbeabd
Marcin Zaremski <marcinzara@gmail.com>
2024-12-02 14:07:08 +0100

    add utm source to footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -3,8 +3,7 @@ import { Link } from "@remix-run/react";
 export default function Footer() {
   return (
     <div style={{ width: '100%', textAlign: 'center', fontSize: '0.9rem', paddingBottom: '1rem' }}>
-      Get one for you – <Link to="/persite">Persite</Link>
-      {/* Get one for you – <a href="https://mzaremski.com/persite?utm_source=persite">Persite</a> */}
+      Get one for you – <Link to={`https://mzaremski.com/persite?utm_source=${process.env.SITE_URL}`}>PerSite</Link>
     </div>
   );
 }
