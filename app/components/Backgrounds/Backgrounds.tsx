ca30bdf957b918853590cb2296a0b75288b31648
Marcin Zaremski <marcinzara@gmail.com>
2024-12-02 16:29:46 +0100

    fix paralaxy stars by multiplying the background on 4k

--- app/components/Backgrounds/Backgrounds.tsx
+++ app/components/Backgrounds/Backgrounds.tsx
@@ -1,9 +1,16 @@
 export const ParallaxyStars = () => {
   return (
     <div id="background-parallaxy-stars">
-      <div></div>
-      <div></div>
-      <div></div>
+      <div className="background-parallaxy-stars">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="background-parallaxy-stars background-parallaxy-stars__second-half">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   )
 } 
